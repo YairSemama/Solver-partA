fd27c69ee8d04ffab8f774763a297bc972a9b5dd
YairSemama <55833857+YairSemama@users.noreply.github.com>
2020-04-26 09:57:45 +0300

    add the files

---
+++ solver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yair on 26/04/2020.
+//
+
+#include "solver.hpp"
---
+++ solver.hpp
@@ -0,0 +1,14 @@
+//
+// Created by yair on 26/04/2020.
+//
+
+#ifndef SOLVER_A_SOLVER_H
+#define SOLVER_A_SOLVER_H
+
+
+class solver {
+
+};
+
+
+#endif //SOLVER_A_SOLVER_H
